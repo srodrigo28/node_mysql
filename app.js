4b41121b7f210db459fb0691f9a0f0373baf30f8
srodrigo28 <rodrigoexer1@gmail.com>
2022-02-25 11:22:58 -0300

    update inserir

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express"); // chama o express para subir o nodemon
 const Usuario = require('./models/Usuario');
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     return res.json({
         erro: false,
@@ -10,6 +12,33 @@ app.get("/", (req, res) => {
         email: "rodrigoexer1@gmail.com"
     });
 });
+
+app.get("/usuario/:id", (req, res) => {
+    const {id} = req.params;
+    return res.json({
+        erro: false,
+        id, name: "ana cicilia", email: "anaci@bol.com"
+    })
+})
+
+app.post("/user", async (req, res) => {
+    const { name, email } = req.body;
+    await Usuario.create(req.body)
+    .then(()  => { 
+        return res.json({
+            erro: false,
+            mensagem: "Usuário Cadastrado com sucesso!"
+        })
+    })
+    .catch(() => {
+    return res.status(400).json({
+        erro: true,
+        mensagem: ":( Não salvo"
+    });
+    });
+});
+
+
 
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080")
